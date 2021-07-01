209f292b336da9ba473f89212cb8a2c35af6e833
Hugo <hugosousa1707@protonmail.com>
2021-07-01 17:20:39 -0300

    Update slow middleware

--- middleware/slow.go
+++ middleware/slow.go
@@ -7,7 +7,6 @@ import (
 
 // Slow the execution handler
 func Slow(h http.Handler, duration time.Duration) http.Handler {
-	tooSlow := time.NewTimer(1 * time.Second)
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		time.Sleep(duration)
 		h.ServeHTTP(rw, r)
